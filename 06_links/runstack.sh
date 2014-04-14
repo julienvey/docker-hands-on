061589f5fc728a1e6cb2d6e6196066f8e75e5657
Julien Vey <vey.julien@gmail.com>
2014-04-14 19:11:04 +0200

    Update Jenkins and run-stack script

--- 06_links/runstack.sh
+++ 06_links/runstack.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-GIT_ID=$(docker run -d git)
+GIT_ID=$(docker run -d julienvey/git)
 GIT_IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $GIT_ID)
 GIT_NAME=$(docker inspect --format='{{.Name}}' $GIT_ID | cut -c 2-)
 HOST_FILE=/tmp/jenkins.host
-JENKINS_ID=$(docker run -p 8080 --link $GIT_NAME:git -d jenkins)
+JENKINS_ID=$(docker run -p 8080 --link $GIT_NAME:git -d julienvey/jenkins)
 JENKINS_IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $JENKINS_ID)
 JENKINS_PORT=$(docker port $JENKINS_ID 8080)
 echo $JENKINS_IP > $HOST_FILE
